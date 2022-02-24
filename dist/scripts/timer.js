2cd9039db1b453cf8429472e7c2b11d5defc12f5
OmidRasouli <o.rasouli92@gmail.com>
2022-02-25 00:12:57 +0330

    Add new Class Digital Number

--- dist/scripts/timer.js
+++ dist/scripts/timer.js
@@ -2,39 +2,19 @@
 
 class Timer {
   //List for second lines [Tens[], Ones[]]
-  secondsLines = [];
-  //The pattern of the numbers which lines must be shown
-  numberPatterns = [
-    [1, 2, 3, 5, 6, 7], //0
-    [6, 7], //1
-    [2, 3, 4, 5, 6], //2
-    [3, 4, 5, 6, 7], //3
-    [1, 4, 6, 7], //4
-    [1, 3, 4, 5, 7], //5
-    [1, 2, 3, 4, 5, 7], //6
-    [3, 6, 7], //7
-    [1, 2, 3, 4, 5, 6, 7], //8
-    [1, 3, 4, 5, 6, 7], //9
-  ];
+  lines = [];
   gameStopped = false;
   //Exec once to store lines in lists
   constructor() {
-    this.secondsLines.push(
-      document.querySelectorAll("#seconds-100 div[data-edge]")
-    );
-    this.secondsLines.push(
-      document.querySelectorAll("#seconds-10 div[data-edge]")
-    );
-    this.secondsLines.push(
-      document.querySelectorAll("#seconds-1 div[data-edge]")
-    );
+    this.lines.push(document.querySelectorAll("#seconds-100 div[data-edge]"));
+    this.lines.push(document.querySelectorAll("#seconds-10 div[data-edge]"));
+    this.lines.push(document.querySelectorAll("#seconds-1 div[data-edge]"));
+
+    this.digitalNumber = new DigitalNumber();
+
     this.time = 0;
 
     this.countDown();
-  }
-  //Reset number lines that must be turned off
-  reset(list) {
-    list.forEach((x) => x.classList.remove("show-number"));
   }
 
   //Timer function (this is obvious :D)
@@ -49,25 +29,14 @@ class Timer {
     //Calc ones of the seconds
     let secondsOnes = Math.trunc(this.time % 10);
 
-    //Reset all numbers
-    this.reset(this.secondsLines[0]);
-    this.reset(this.secondsLines[1]);
-    this.reset(this.secondsLines[2]);
-
     //Show number (hundreds - seconds)
-    this.numberPatterns[secondsHundreds].forEach((x) =>
-      this.secondsLines[0][x - 1].classList.add("show-number")
-    );
+    this.digitalNumber.showNumber(secondsHundreds, this.lines[0]);
 
     //Show number (tens - seconds)
-    this.numberPatterns[secondsTens].forEach((x) =>
-      this.secondsLines[1][x - 1].classList.add("show-number")
-    );
+    this.digitalNumber.showNumber(secondsTens, this.lines[1]);
 
     //Show number (ones - seconds)
-    this.numberPatterns[secondsOnes].forEach((x) =>
-      this.secondsLines[2][x - 1].classList.add("show-number")
-    );
+    this.digitalNumber.showNumber(secondsOnes, this.lines[2]);
 
     let self = this;
     //Call this function after 1s
